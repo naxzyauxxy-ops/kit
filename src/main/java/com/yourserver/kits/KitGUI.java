30aec852c38f71a6ae5d7cc22f8f9b62b16d2c78
naxzyauxxy-ops <naxzyauxxy@gmail.com>
2026-03-25 14:24:51 -0400

    Update KitGUI.java

--- src/main/java/com/yourserver/kits/KitGUI.java
+++ src/main/java/com/yourserver/kits/KitGUI.java
@@ -2,7 +2,9 @@ package com.yourserver.kits;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
-import org.bukkit.command.*;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
@@ -11,6 +13,8 @@ public class KitGUI implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        if (!(sender instanceof Player)) return true;
 
         Player player = (Player) sender;
 
@@ -24,3 +28,4 @@ public class KitGUI implements CommandExecutor {
 
         return true;
     }
+}
